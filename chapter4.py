ca4cbd2d49f4500d6797f4cf9e7d179edce72c36
mateuszcygan <mateuszcygan@protonmail.com>
2024-10-08 18:33:33 +0200

    Review exercise 4 in chapter 4

---
+++ chapter4.py
@@ -0,0 +1,5 @@
+# 4. Print the string "zing" by using slice notation on the string "bazinga" to specify the correct range of characters.
+
+example = "bazinga"
+part_of_example = example[2:]
+print(part_of_example)
