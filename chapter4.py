3877ff350cc62669b528dc8bc159eecc6ab556ec
mateuszcygan <mateuszcygan@protonmail.com>
2024-10-09 18:01:23 +0200

    an exercise with lower() function completed

--- chapter4.py
+++ chapter4.py
@@ -1,5 +1,11 @@
-# 4. Print the string "zing" by using slice notation on the string "bazinga" to specify the correct range of characters.
+# E4 Print the string "zing" by using slice notation on the string "bazinga" to specify the correct range of characters.
 
-example = "bazinga"
-part_of_example = example[2:]
-print(part_of_example)
+# example = "bazinga"
+# part_of_example = example[2:]
+# print(part_of_example)
+
+# E1 P87
+# lesson take: strings are immutable, lower() returns a new string
+strings = ["Animals", "Badger", "Honey Bee", "Honeybadger"]
+for string in strings:
+    print(string.lower())
